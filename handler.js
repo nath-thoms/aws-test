8886acc73392f77d6979f31c5abc7dd1acd46673
nath <nath.chadwick@gmail.com>
2018-09-02 16:24:40 +0100

    added cron request

--- handler.js
+++ handler.js
@@ -1,20 +1,10 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.hello = (event, context, callback) => {
   
-  console.log(event.pathParameters.name);
+  const now = new Date();
+  const message = `The time is ${now}`;
+  console.log(message);
 
-  const name = event.pathParameters.name;
-  const reply = `Hello there, ${name}!`
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      //message: 'Go Serverless v1.0! Your function executed successfully!',
-      message: JSON.stringify(reply)
-    }),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, message);
 };
